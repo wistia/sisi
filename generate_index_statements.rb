3e82111097374ecc5d9a6cbbe9884bdb8c546f21
Robby Grossman <robby@freerobby.com>
2013-11-20 16:29:34 -0500

    Add a script to generate index drops/creations based on a sql schema.

---
+++ generate_index_statements.rb
@@ -0,0 +1,65 @@
+
+class IndexStatementExtractor
+  def extract(filepath)
+    table_indices = extract_statements(filepath)
+    puts
+    puts 'DROP statements:'
+    puts
+    indices_to_drops(table_indices).each do |stmt|
+      puts stmt
+    end
+    puts
+    puts 'CREATE statements:'
+    puts
+    indices_to_creates(table_indices).each do |stmt|
+      puts stmt
+    end
+  end
+
+  private
+  def indices_to_drops(table_indices)
+    statements = []
+    table_indices.each do |table, data|
+      data.each do |t|
+        t =~ /.*KEY\s+\`(\S+)\`.*/
+        index = $1
+        statements << "DROP INDEX `#{index}` ON #{table};"
+      end
+    end
+    statements
+  end
+  def indices_to_creates(table_indices)
+    statements = []
+    table_indices.each do |table, data|
+      statements << '' << "ALTER TABLE #{table}" if data.count > 0
+      data.each do |t|
+        t =~ /\s*(([A-Z][A-Z\ ])*\ KEY)\s+\`(\S+)\`\s+\((.*)\)/
+        statements << "ADD #{$1} \`#{$3}\` (#{$4}),".gsub('KEY', 'INDEX')
+      end
+      statements[-1][-1] = ';' if data.count > 0
+    end
+    statements
+  end
+  def extract_statements(filepath)
+    table_datas = {}
+    current_table = nil
+    File.open(filepath, 'r').each_line do |line|
+      if line =~ /CREATE TABLE `(.*)` \(/
+        current_table = $1
+      elsif line =~ /\) ENGINE=/
+        current_table = nil
+      else
+        if current_table
+          table_datas[current_table] ||= []
+          table_datas[current_table] << line if line.include?('KEY') && !line.include?('PRIMARY')
+        end
+      end
+    end
+    table_datas
+  end
+end
+
+if __FILE__ == $0
+  extractor = IndexStatementExtractor.new
+  extractor.extract(ARGV[0])
+end
